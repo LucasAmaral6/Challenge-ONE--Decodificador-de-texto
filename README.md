a92b1fb141f70bdf076a73c99f66370571ce9234
Lucas Amaral <107057807+LucasAmaral6@users.noreply.github.com>
2024-03-03 22:11:55 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Challenge-ONE--Decodificador-de-texto
+
+📝Sobre 
+
+Este projeto trata-se de um decodificador de Texto usando JavaScript para criptografar e descriptografar mensagens usando um conjunto específico de substituições.
+
+🚀Tecnologias
+<div>
+  <img loading="lazy" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="40" height="40"/>
+  <img loading="lazy" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="40" height="40"/>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="40" height="40"/>
+</div>
+
+💼 Link do projeto no Github Pages
+
---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+let botaoDeCriptografar = document.getElementById("encrypt-button");
+let botaoDeDescriptografar = document.getElementById("decrypt-button");
+let botaoDeCopiar = document.getElementById("output-copy-button");
+let body = document.querySelector("body");
+
+function showInitialOutput() {
+    let initialOutput = document.querySelector(".content-output-details");
+    let hidOutput = document.querySelector(".output-decoded");
+    hidOutput.style.display = "none";
+    initialOutput.style.display = "block";
+}
+
+function copy() {
+    let txtToCopy = document.querySelector("#output-text").innerHTML;
+    navigator.clipboard.writeText(txtToCopy);
+}
+
+function cleanOutputArea() {
+    
+    let initialOutput = document.querySelector(".content-output-details");
+    initialOutput.style.display = "none";
+}
+
+function getInput() {
+    let stringInput = document.querySelector(".content-input-text");
+    let string = stringInput.value;
+    return string;
+}
+
+function addTextOutput() {
+    
+    let showOutput = document.querySelector(".output-decoded");
+    showOutput.style.display = "flex";
+}
+
+function showOutputText(stringToShow) {
+    
+    let tagOutputText = document.querySelector(".output-text");
+    tagOutputText.innerHTML = stringToShow;
+}
+
+function encrypt(stringToEncrypt) { 
+    
+    let encryptedString = 
+    stringToEncrypt
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+
+    cleanOutputArea();  
+    addTextOutput();
+    showOutputText(encryptedString)
+    return encryptedString;
+}
+
+function decrypt(encryptedString) {
+
+    let decryptedString = 
+    encryptedString
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+
+    cleanOutputArea();
+    addTextOutput();
+    showOutputText(decryptedString)
+    return decryptedString;
+}
+
+botaoDeCriptografar.addEventListener("click", (e) => {
+    let frase = getInput();
+    if (frase != "") {
+        encrypt(frase);
+    } else {
+        showInitialOutput();
+    }
+});
+botaoDeDescriptografar.addEventListener("click", (e) => {
+    let frase = getInput();
+    if (frase != "") {
+        if (frase.includes("enter")||frase.includes("imes")||frase.includes("ai")||frase.includes("ober")||frase.includes("ufat")) {
+            decrypt(frase);
+        } else {
+            showInitialOutput();
+        }        
+    } else {
+        showInitialOutput();
+    }
+});
+
+botaoDeCopiar.addEventListener("click", copy);
